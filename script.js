4031313e6592497ac6b9184f46e3ded434f6038f
Shalom Raichman <shalomr1770@gmail.com>
2024-08-14 11:30:39 +0300

    "commit"

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //alert("welcom")
 
 
-const missiles = []
+const missiles;
 
 const addMIssile = () => {
     const missilesDiv = document.getElementById("missiles-div")
@@ -11,24 +11,48 @@ const addMIssile = () => {
     missilesDiv.appendChild(p)
 }
 let result;
- 
+
+let j = 0;
+
 const loadMissilesjson = async() => {
 
     const response = await fetch("/missiles.json");
     result = await response.json();
 
-    console.log(missiles);
-    
-    
+    if (j < 1) {
+        const missilesDiv = document.getElementById("div1")
+        const p = document.createElement("p")
+        p.innerText = "Missiles loaded"
+
+        missilesDiv.appendChild(p)
+        j++;
+    }
 }
 
-const socket = new WebSocket('ws://localhost:3108/MissileHandler');
+const socket = new WebSocket('ws://localhost:3108/MissileHanlder');
 
-let i = 0;
+const shutMissiles = () => {
 
-const publishMessage = () => {
-    if(i < result.length){
-        socket.send(JSON.stringify(result[i]));
+    if (result.length){
+        socket.send(JSON.stringify(result.shift()));
     }
-  i++;
+    
+
+    const missilesDiv = document.getElementById("div2")
+    const p = document.createElement("p")
+    p.innerText = `Missile ${result[0]["name"]} has been shut!!`
+
+    missilesDiv.appendChild(p)
+}
+
+// Handle messages from the backend
+socket.onmessage = (event) => {
+  console.log('got message', JSON.stringify(event.data));
+
+    const missilesDiv = document.getElementById("div3")
+    const p = document.createElement("p")
+    p.innerText = `Missile ${event.missileName} has been ${event.intercepted}!!`
+
+    missilesDiv.appendChild(p)
+
 }
